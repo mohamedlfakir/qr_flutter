483bde69821faf1d8605d5c0bb2579d93de7c18d
mohamedlfakir <mohamedlfakir2493@gmail.com>
2025-03-05 06:31:42 -0800

    first commit

---
+++ ios/RunnerTests/RunnerTests.swift
@@ -0,0 +1,16 @@
+//
+//  RunnerTests.swift
+//  RunnerTests
+//
+//  Created by medboutakat on 5/3/2025.
+//
+
+import Testing
+
+struct RunnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
